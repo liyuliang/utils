45ba379c21e5106c08fb78921a4770ba30cd3d7e
liang <liyuliang2013@gmail.com>
2017-10-03 14:14:08 +0800

    add url decode function

--- format/url.go
+++ format/url.go
@@ -6,12 +6,10 @@ import (
 )
 
 func UrlDecode(url string) (string, error) {
-	if url == ""{
-		return "",errors.New("miss argument url")
+	if url == "" {
+		return "", errors.New("miss argument url")
 	}
-	u, err := URL.Parse(url)
-	if err != nil {
-		return "", err
-	}
-	return u.Path, err
+
+	url, err := URL.QueryUnescape(url)
+	return url, err
 }
