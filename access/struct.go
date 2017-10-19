b96b1ddffa1cabfe6d89575138133202f52417f2
liang <liyuliang2013@gmail.com>
2017-10-19 14:32:55 +0800

    fixed access set field method

--- access/struct.go
+++ access/struct.go
@@ -2,40 +2,30 @@ package access
 
 import (
 	"reflect"
-	"errors"
 	"strings"
 )
 
-func SetField(obj interface{}, name string, value interface{}) error {
+func SetField(obj interface{}, name string, value interface{}) {
 	name = strings.Title(name)
 
 	structValue := reflect.ValueOf(obj).Elem()
 	structFieldValue := structValue.FieldByName(name)
 
-	if !structFieldValue.IsValid() {
-		return errors.New("No such field: " + name + " in obj")
-	}
+	if structFieldValue.IsValid() {
 
-	if !structFieldValue.CanSet() {
-		return errors.New("Cannot set " + name + " field value")
-	}
+		if structFieldValue.CanSet() {
+			structFieldType := structFieldValue.Type()
+			val := reflect.ValueOf(value)
 
-	structFieldType := structFieldValue.Type()
-	val := reflect.ValueOf(value)
-	if structFieldType != val.Type() {
-		return errors.New("Provided value type didn't match obj field type")
-	}
-
-	structFieldValue.Set(val)
-	return nil
-}
-
-func Set(obj interface{}, m map[string]interface{}) error {
-	for k, v := range m {
-		err := SetField(obj, k, v)
-		if err != nil {
-			return err
+			if structFieldType == val.Type() {
+				structFieldValue.Set(val)
+			}
 		}
 	}
-	return nil
+}
+
+func Set(obj interface{}, m map[string]interface{}) {
+	for k, v := range m {
+		SetField(obj, k, v)
+	}
 }
